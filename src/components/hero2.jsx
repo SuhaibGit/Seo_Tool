9db01565e8031a29348f7594bfca43620db00bba
SuhaibGit <m.suhaib1906@gmail.com>
2025-04-03 21:59:08 +0500

    updated

---
+++ src/components/hero2.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../style.css'
+
+const Hero2 = () => {
+  return (
+    <section className='hm'>
+        <div className='hmh'>
+            <div className='hmhs'>
+                <h1> More than just a&nbsp;<span> grid tracker</span> </h1>
+            </div>
+
+            <div className='stats-container'>
+                <div className='hms'>
+                    <h1>5.5M</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste deserunt assumenda veniam recusandae sint natus accusantium blanditiis quas error quibusdam, itaque dicta explicabo praesentium vero excepturi vitae fugiat soluta? Beatae.</p>
+                </div >
+                <div className='hms2'>
+                    <h1>5x</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste deserunt assumenda veniam recusandae sint natus accusantium blanditiis quas error quibusdam, itaque dicta explicabo praesentium vero excepturi vitae fugiat soluta? Beatae.</p>
+                </div>
+                
+                <div className='hms'>
+                    <h1>89%</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste deserunt assumenda veniam recusandae sint natus accusantium blanditiis quas error quibusdam, itaque dicta explicabo praesentium vero excepturi vitae fugiat soluta? Beatae.</p>
+                </div>
+                <div className='hms2'>
+                    <h1>5x</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste deserunt assumenda veniam recusandae sint natus accusantium blanditiis quas error quibusdam, itaque dicta explicabo praesentium vero excepturi vitae fugiat soluta? Beatae.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero2
