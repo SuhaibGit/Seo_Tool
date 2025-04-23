c6a7fed014eb5242b24d7742e963a93814e61334
ezhanjaved <ezhan@localityagency.com>
2025-04-23 21:02:07 +0500

    Update hero2.jsx

--- src/components/hero2.jsx
+++ src/components/hero2.jsx
@@ -10,21 +10,21 @@ const Hero2 = () => {
             </div>
             <div className='stats-container'>
                 <div className='hms'>
-                    <h2>10s</h2>
-                    <p>is all it needs to crawl a site</p>
+                    <h2>30s</h2>
+                    <p>is all it needs to crawl and gather insights</p>
                 </div >
                 <div className='hms2'>
-                    <h2>6x</h2>
+                    <h2>5x</h2>
                     <p>speed of what a normal tracker has on market</p>
                 </div>
                 
                 <div className='hms'>
-                    <h2>89%</h2>
-                    <p>accurate than the nearest competitors</p>
+                    <h2>22%</h2>
+                    <p>higher accuracy than the nearest competitors</p>
                 </div>
                 <div className='hms2'>
                     <h2>2x</h2>
-                    <p>powerful and accurate than the local falcon</p>
+                    <p>more powerful than the leading competitor</p>
                 </div>
             </div>
         </div>
